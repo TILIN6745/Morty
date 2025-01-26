d95254e0fc22d61f15b6eada7df8bef7332b3a95
Tilin <jm293229@gmail.com>
2025-01-26 11:05:03 -0600

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -7,9 +7,9 @@ let pesan = args.join` `
 let oi = `📄 *MENSAJE:* ${pesan}`
 let teks = `╭━〔 *EY LES HABLAN ACA EL ÚNICO BOT SOY YO* 〕━⬣\n${oi}\n\n`
 for (let mem of participants) {
-teks += `┃🥷⊹ @${mem.id.split('@')[0]}\n`}
+teks += `┃💳⊹ @${mem.id.split('@')[0]}\n`}
 teks += `┃
-╰━━━━━[ 𝗧𝗜𝗟𝗜𝗡 𝗕𝗢𝗧 ]━━━━━⬣`
+╰━━━━━[ 𝗠𝗔𝗥𝗧𝗬 ]━━━━━⬣`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
 handler.command = /^(tagall|niñas|invocar|invocacion|todos|invocación)$/i
